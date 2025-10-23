92d28082d48fe1d2067dcfdea77f099faada8ac3
schluchtenscheisser <martinteiber@gmx.at>
2025-10-23 11:42:24 +0200

    Update test_imports.py

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -1,1 +1,18 @@
+def test_imports():
+    import colabtool
+    from colabtool import (
+        utils,
+        data_sources,
+        pre_universe,
+        exchanges,
+        features,
+        scores,
+        breakout,
+        buzz,
+        backtest,
+        export,
+        export_helpers,
+        category_providers,
+        cg_cache_patch,
+    )
 
