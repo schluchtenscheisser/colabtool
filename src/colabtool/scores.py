55ba154b4bee8d9a340e94221cabec65d68a15bd
schluchtenscheisser <martinteiber@gmx.at>
2025-12-04 21:53:58 +0100

    Rename import from utils to utilities

--- src/colabtool/scores.py
+++ src/colabtool/scores.py
@@ -3,7 +3,7 @@
 from __future__ import annotations
 
 from typing import Optional
-from .utils import pd, np, logging
+from .utilities import pd, np, logging
 
 # ----------------------------
 # Helpers
